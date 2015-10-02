a7a167c19ba03fcdf118c07345fa3656f09369ea
Paul Kronenwetter <kronenpj@gmail.com>
2015-10-02 17:39:33 -0400

    Refactoring to make explicit D-to-A and PTT pins & ports, and turning on and off PTT. This should also help to make porting to another chip easier.

--- device.h
+++ device.h
@@ -38,8 +38,17 @@
     #define DAC_DDR  DDRD
     #define LED_PORT PORTB
     #define LED_DDR  DDRB
+    #define PTT_PORT PORTD
+    #define PTT_DDR  DDRD
     #define ADC_PORT PORTC
     #define ADC_DDR  DDRC
+    // Pins 3-7 on Port D = Arduino D3 - D7
+    #define DAC_HIGH _BV(7)
+    #define DAC_PINS _BV(7)&_BV(6)&_BV(5)&_BV(4)
+    #define LED_TX   1    // Arduino D9
+    #define LED_RX   2    // Arduino D10
+    #define PTT_TX   3    // Arduino D11
+    #define ADC_NO   0    // Arduino A0
 #endif
 
 #endif
